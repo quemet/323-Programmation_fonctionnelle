23d80cdccbbf994c03ecadb846ee642d9eb6e145
Quentin <quentinmet7@gmail.com>
2024-09-27 10:46:45 +0200

    feat(exo maximum): Remplacer les variables mutables par immutables

--- personnel/maximum/Program.cs
+++ personnel/maximum/Program.cs
@@ -1,42 +1,55 @@
-﻿// 4 players
-List<Player> players = new List<Player>()
-{
-    new Player("Joe", 32),
-    new Player("Jack", 30),
-    new Player("William", 37),
-    new Player("Averell", 25)
-};
+﻿using System.Collections.Immutable;
 
-// Initialize search
-Player elder = players.First();
-int biggestAge = elder.Age;
-
-// search
-foreach (Player p in players)
+namespace maximum
 {
-    if (p.Age > biggestAge) // memorize new elder
+    class Program
     {
-        elder = p;
-        biggestAge = p.Age; // for future loops
-    }
-}
-
-Console.WriteLine($"Le plus agé est {elder.Name} qui a {elder.Age} ans");
-
-Console.ReadKey();
-
-public class Player
-{
-    private readonly string _name;
-    private readonly int _age;
-
-    public Player(string name, int age)
-    {
-        _name = name;
-        _age = age;
+        static void Main(string[] args)
+        {
+            Player.GetElder();
+        }
     }
 
-    public string Name => _name;
+    public class Player
+    {
+        private readonly string _name;
+        private readonly int _age;
 
-    public int Age => _age;
+        public Player(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public string Name => _name;
+
+        public int Age => _age;
+
+        public static void GetElder()
+        {
+            // 4 players
+            ImmutableList<Player> players = [new Player("Joe", 32), new Player("Jack", 30), new Player("William", 37), new Player("Averell", 25)];
+
+            // Initialize search
+            Player elder = new("A", -1);
+
+            // Player eld = players.Aggregate((c, n) => c.Age > n.Age ? c : n);
+
+            // int biggestAge = elder.Age;
+
+            // search
+            foreach (Player p in players)
+            {
+                if (p.Age > elder.Age) // memorize new elder
+                {
+                    elder = new Player(p.Name, p.Age);
+                    // biggestAge = p.Age; // for future loops
+                }
+            }
+
+            Console.WriteLine($"Le plus agé est {elder.Name} qui a {elder.Age} ans");
+
+            Console.ReadKey();
+        }
+    }
 }
